69c9c32ab5e5f2a3541523c3e0c89a6608aae801
lpanavas <71109392+lpanavas@users.noreply.github.com>
2020-11-10 15:55:10 -0500

    Initial Commit

---
+++ Assets/Scripts/spawnCube.cs
@@ -0,0 +1,31 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+
+public class spawnCube : UdonSharpBehaviour
+{
+    public GameObject spawnItem;
+    public GameObject audioObject;
+
+    float sphereRadius = 10;
+
+
+    public override void Interact()
+    {
+
+
+
+        for (int i = 0; i < 250; i++)
+        {
+            var newObject = VRCInstantiate(spawnItem);
+            Vector3 onPlanet = Random.onUnitSphere * sphereRadius;
+            newObject.transform.position = onPlanet;
+        }
+        var newAudioObject = VRCInstantiate(audioObject);
+        newAudioObject.transform.position = new Vector3((Random.Range(-10.0f, 10.0f)), Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
+
+    }
+}
---
+++ Assets/Scripts/triggerSound.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class triggerSound : UdonSharpBehaviour
+{
+    public GameObject audioObject;
+
+    public override void Interact()
+    {
+        // audio = audioObject.GetComponent<AudioSource>();
+        // audio.enabled = !audio.enabled;
+        audioObject.SetActive(!audioObject.activeSelf);
+        Debug.Log("hello");
+
+    }
+}
